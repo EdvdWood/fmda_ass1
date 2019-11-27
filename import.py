695067f1c2e3b259272ea33bc691c45609e55781
Sagarmatra <ejvdhout@vivaldi.net>
2019-11-27 11:33:12 +0100

    splitting and filtering data

--- import.py
+++ import.py
@@ -6,6 +6,35 @@ import numpy as np
 
 dataset = pd.read_csv("data.csv")
 
+#%% Splitting
+
+socio_set = dataset[dataset["Indicator"]=="1.1.1"]
+socio_set = socio_set[socio_set["Nature"]=="G"]
+nat_set = dataset[dataset["Indicator"]=="7.1.2"]
+
+#%% Filtering for most recent year
+
+socio_filter = socio_set.groupby(["GeoAreaName","TimePeriod"])["Value"].first().reset_index()
+nat_filter = nat_set.groupby(["GeoAreaName","TimePeriod"])["Value"].first().reset_index()
+
+yearid = pd.merge(socio_filter, nat_filter, how="inner", on="GeoAreaName")
+yearid = yearid[yearid["TimePeriod_x"]==yearid["TimePeriod_y"]]
+
+yearselected = yearid.groupby(["GeoAreaName"])["TimePeriod_x"].max().reset_index()
+
+del socio_filter, nat_filter, yearid
+
+#%%
+socio_subset = pd.merge(yearselected, socio_set, how="left", left_on=["GeoAreaName", "TimePeriod_x"], right_on=["GeoAreaName","TimePeriod"])
+nat_subset = pd.merge(yearselected, nat_set, how="left", left_on=["GeoAreaName", "TimePeriod_x"], right_on=["GeoAreaName","TimePeriod"])
+
+del yearselected, nat_set, socio_set
+
+#%%
+socio_subset = socio_subset.sort_values(by="GeoAreaName")
+nat_subset = socio_subset.sort_values(by="GeoAreaName")
+
+
 #%% 
 
 def grabber(file):
