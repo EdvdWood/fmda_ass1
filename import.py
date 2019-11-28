dff9b00049e889890fb12d1cedcd466537af694c
Sagarmatra <ejvdhout@vivaldi.net>
2019-11-28 15:25:56 +0100

    Optimized code. TODO: Refactoring/Prettyfying

--- import.py
+++ import.py
@@ -8,7 +8,7 @@ import scipy.stats as stats
 
 #%% Fast way
 
-dataset = pd.read_csv("data.csv")
+dataset = pd.read_csv("data.csv")[["Indicator", "GeoAreaName", "TimePeriod", "Value", "Nature"]]
 
 
 #%% Splitting
@@ -17,21 +17,19 @@ socio_set = dataset[dataset["Indicator"]=="1.1.1"]
 socio_set = socio_set[socio_set["Nature"]=="G"]
 nat_set = dataset[dataset["Indicator"]=="7.1.2"]
 
+del dataset
 #%% Filtering for most recent year
 
-socio_filter = socio_set.groupby(["GeoAreaName","TimePeriod"])["Value"].first().reset_index()
-nat_filter = nat_set.groupby(["GeoAreaName","TimePeriod"])["Value"].first().reset_index()
-
-yearid = pd.merge(socio_filter, nat_filter, how="inner", on="GeoAreaName")
+yearid = pd.merge(socio_set, nat_set, how="inner", on="GeoAreaName")
 yearid = yearid[yearid["TimePeriod_x"]==yearid["TimePeriod_y"]]
 
 yearselected = yearid.groupby(["GeoAreaName"])["TimePeriod_x"].max().reset_index()
 
-del socio_filter, nat_filter, yearid
+del yearid
 
 #%%
-socio_subset = pd.merge(yearselected, socio_set, how="left", left_on=["GeoAreaName", "TimePeriod_x"], right_on=["GeoAreaName","TimePeriod"])
-nat_subset = pd.merge(yearselected, nat_set, how="left", left_on=["GeoAreaName", "TimePeriod_x"], right_on=["GeoAreaName","TimePeriod"])
+socio_subset = pd.merge(yearselected, socio_set, how="left", left_on=["GeoAreaName", "TimePeriod_x"], right_on=["GeoAreaName","TimePeriod"]).drop(["TimePeriod_x"], axis=1)
+nat_subset = pd.merge(yearselected, nat_set, how="left", left_on=["GeoAreaName", "TimePeriod_x"], right_on=["GeoAreaName","TimePeriod"]).drop(["TimePeriod_x"], axis=1)
 
 del yearselected, nat_set, socio_set
 
@@ -45,17 +43,14 @@ nat_subset["Value"] = nat_subset["Value"].map(lambda x: x.lstrip('<>'))
 nat_subset["Value"] = nat_subset["Value"].astype("float")/100
 
 #%% Sanity Check
-print("Checking Social Dataset")
-print(socio_subset[["TimePeriod", "Value"]].describe())
-print("\nChecking Natural Dataset")
-print(nat_subset[["TimePeriod", "Value"]].describe())
 
-#%% Sanity Check Part 2: Boxplots
+def sanchecker(df1, df2):
+    if df1["GeoAreaName"].equals(df2["GeoAreaName"]):
+        return("DataFrame Areas Match")
+    else: 
+        return("DataFrame Area Mismatch")
 
-#fig, (ax1, ax2) = plt.subplots(1,2)
-#ax1.boxplot([socio_subset["Value"],nat_subset["Value"]])
-#ax2.boxplot([socio_subset["TimePeriod"],nat_subset["TimePeriod"]])
-#plt.show()
+print(sanchecker(socio_subset, nat_subset))
 
 #%% Setting Up Main Plot
 fig, ax = plt.subplots(1,1)
@@ -101,9 +96,9 @@ def significancecheck(pvalue):
 
 def tradeoff(corr):
     if corr < 0:
-        return("\nThe negative correlation implies that a reduction in Proportion of population below international poverty line is paired with an increase in the Proportion of population with primary reliance on clean fuels and technology")
+        return("\nThe negative correlation implies that a reduction in Proportion of population below international poverty line is paired with an increase in the Proportion of population with primary reliance on clean fuels and technology, implying a synergy between the two SDG's")
     elif corr > 0: 
-        return("\nThe positive correlation implies that an increase in Proportion of population below international poverty line is paired with an increase in the Proportion of population with primary reliance on clean fuels and technology")
+        return("\nThe positive correlation implies that an increase in Proportion of population below international poverty line is paired with an increase in the Proportion of population with primary reliance on clean fuels and technology, implying a tradeoff between the two SDG's")
     else:
         return("\nThere is no correlation")
 #%% Printing Outputs
